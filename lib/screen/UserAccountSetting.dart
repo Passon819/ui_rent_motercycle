457f2f33bc40bd46ee5dda7421e528b6aab30ddf
Passon819 <passon871@gmail.com>
2023-07-03 13:45:19 +0700

    edit UserAccountSetting

--- lib/screen/UserAccountSetting.dart
+++ lib/screen/UserAccountSetting.dart
@@ -16,6 +16,10 @@ class _UserAccountSettingState extends State<UserAccountSetting> {
       TextEditingController(text: '0987654321');
   TextEditingController AddressController = TextEditingController(
       text: '128 ถ.ห้วยแก้ว ต.ช้างเผือก อ.เมือง จ.เชียงใหม่ 50300');
+  TextEditingController LatitudeController =
+      TextEditingController(text: '18.123456');
+  TextEditingController LongitudeController =
+      TextEditingController(text: '98.123456');
 
   @override
   Widget build(BuildContext context) {
@@ -132,80 +136,115 @@ class _UserAccountSettingState extends State<UserAccountSetting> {
                     )),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(50, 10, 50, 15),
-              child: TextFormField(
-                controller: AddressController,
-                minLines: 2,
-                maxLines: 3,
-                keyboardType: TextInputType.multiline,
-                decoration: InputDecoration(
-                    filled: true, //<-- SEE HERE
-                    fillColor: Colors.white, //<-- SEE HERE
-                    hintText: 'ที่อยู่บนแผนที่ Google Maps',
-                    labelText: 'ที่อยู่บนแผนที่ Google Maps',
-                    hintStyle: TextStyle(color: Colors.grey[600]),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(10)),
-                    )),
-              ),
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                  minimumSize: Size(100, 40)),
-              child: const Text(
-                'ค้นหาตำแหน่งร้านของคุณ',
-                style: TextStyle(fontSize: 16),
-              ),
-            ),
+            SizedBox(height: 10),
+            //กล่อง location
             Container(
-              height: 55,
-              width: 300,
+              height: 80,
+              width: 310,
               decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(10),
-                  border: Border.all(width: 1, color: Colors.grey)),
-              child: Row(
+                  border: Border.all(width: 1, color: Colors.black)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                        ),
-                        //minimumSize: Size(100, 40)
-                      ),
-                      child: const Text(
-                        'ค้นหา',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ),
+                  Text(
+                    'ที่อยู่บนแผนที่ Google Maps',
+                    style: TextStyle(fontSize: 12, color: Colors.grey[600]),
                   ),
-                  Expanded(
-                    child: TextFormField(
-                      onChanged: (value) {
-                        setState(() {
-                          // origPrice = double.parse(
-                          //     value); //แปลงข้อความvalueให้เป็นdouble
-                          // calStatus = false; //เปลี่ยนสถานะเพื่อเคลียร์ผลลัพธ์
-                        });
-                      },
-                      textAlign: TextAlign.center, //ให้ป้อนข้อมูลอยู่ตรงกลาง
-                      style: const TextStyle(color: Colors.black, fontSize: 18),
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(borderSide: BorderSide.none),
-                        hintText: "ชื่อ นามสกุล",
-                        labelText: 'ชื่อ นามสกุล',
-                        hintStyle: TextStyle(fontSize: 14, color: Colors.grey),
+                  Row(
+                    children: [
+                      SizedBox(width: 10),
+                      //ปุ่มตำแหน่ง
+                      ElevatedButton.icon(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0),
+                          ),
+                          //minimumSize: Size(100, 40)
+                        ),
+                        icon: Icon(Icons.gps_fixed),
+                        label: Text('gps'),
                       ),
-                    ),
+                      Expanded(
+                        child: TextFormField(
+                          onChanged: (value) {
+                            setState(() {
+                              // origPrice = double.parse(
+                              //     value); //แปลงข้อความvalueให้เป็นdouble
+                              // calStatus = false; //เปลี่ยนสถานะเพื่อเคลียร์ผลลัพธ์
+                            });
+                          },
+                          enabled: false,
+                          controller: LatitudeController,
+                          textAlign:
+                              TextAlign.center, //ให้ป้อนข้อมูลอยู่ตรงกลาง
+                          style: const TextStyle(
+                              color: Colors.black, fontSize: 14),
+                          decoration: const InputDecoration(
+                            border:
+                                OutlineInputBorder(borderSide: BorderSide.none),
+                            hintText: "18.xxxxxx",
+                            //labelText: '18.xxxxxx',
+                            hintStyle:
+                                TextStyle(fontSize: 14, color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: TextFormField(
+                          onChanged: (value) {
+                            setState(() {
+                              // origPrice = double.parse(
+                              //     value); //แปลงข้อความvalueให้เป็นdouble
+                              // calStatus = false; //เปลี่ยนสถานะเพื่อเคลียร์ผลลัพธ์
+                            });
+                          },
+                          enabled: false,
+                          controller: LongitudeController,
+                          textAlign:
+                              TextAlign.center, //ให้ป้อนข้อมูลอยู่ตรงกลาง
+                          style: const TextStyle(
+                              color: Colors.black, fontSize: 14),
+                          decoration: const InputDecoration(
+                            border:
+                                OutlineInputBorder(borderSide: BorderSide.none),
+                            hintText: "98.xxxxxx",
+                            //labelText: '98.xxxxxx',
+                            hintStyle:
+                                TextStyle(fontSize: 14, color: Colors.grey),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
+              ),
+            ),
+            SizedBox(height: 100),
+            //ปุ่มแก้ไข
+            GestureDetector(
+              onTap: () {
+                setState(() {});
+                //debugPrint(" ");
+              },
+              child: Container(
+                height: 50,
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.green[700],
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: const Center(
+                  child: Text(
+                    'แก้ไข',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ),
               ),
             ),
           ],
